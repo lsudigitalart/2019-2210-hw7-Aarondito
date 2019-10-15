927668d8fb8ddc41bc225c063f2e46913442c826
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-10-15 14:17:19 -0500

    music

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+var 67;
+var playTime;
+var loadTime;
+
+function preload() {
+  67  = loadSound("67.mp3");
+
+}
+
 function setup() {
+    if (67.isLoaded()){
+    67.play();
+    time = millis();
+
+    print (loadTime);
+    67.play();
     // Put things you want to run once in here. 
   }
-  
+}
+
   function draw() {
+      playTime = millis() - loadTime;
+      print(playTime);
+     
+
+      if (playTime > ){
+          background();
+      }
     // Put things you want to over and over here. 
   }
+  // 67 = song name
