5b880adfaa55d3e3e9937631786fa5845aab09be
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-10-22 12:51:04 -0500

    added vid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var key;
 var playTime;
 var loadTime;
-
+var falling;
 function preload() {
   key  = loadSound("key.mp3");
 
@@ -12,13 +12,13 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0)
    
-   
+    
     // Put things you want to run once in here. 
   }
 
 
   function draw() {
-    
+    falling= createVideo(['assets/falling.mp4']);
    
    
     // Put things you want to over and over here. 
