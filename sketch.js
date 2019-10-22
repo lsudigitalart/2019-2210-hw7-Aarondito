94752eb203976d9427e2196fac89ff4c03ab3290
Aarondito <54543739+Aarondito@users.noreply.github.com>
2019-10-22 13:51:02 -0500

    done

--- sketch.js
+++ sketch.js
@@ -1,26 +1,39 @@
 var key;
 var playTime;
 var loadTime;
-var falling;
+var img;
+
 function preload() {
   key  = loadSound("key.mp3");
-
+  img= loadImage('test10.jpg');
 }
 
 function setup() { 
-    key.play ();
-    createCanvas(windowWidth, windowHeight);
-    background(0)
+    key.play();
+    createCanvas(1575, 725);
+    frameRate(1);
    
-    
     // Put things you want to run once in here. 
   }
 
 
   function draw() {
-    falling= createVideo(['assets/falling.mp4']);
+    background(0);
+    image(img, random(width), random(height));
+    scale(0.25);
+    image(img, random(width), random(height));
+    scale(0.13);
    
+
+     //for(var i = 0; i < 6; i++){
+      image(img, random(width), random(height));
+     
+     //}
+    
    
+ 
     // Put things you want to over and over here. 
   }
+
+
   // key = song name
